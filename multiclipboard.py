d3300e2ea3b165cef76587b9a3fa59648027f3db
jay-2000 <jayparmar7654321@gmail.com>
2022-03-25 22:15:30 +0530

    feat: added complete logic for multiclipboard project

--- multiclipboard.py
+++ multiclipboard.py
@@ -19,3 +19,25 @@ def load_data(filepath):
         return {}
 
 
+if len(sys.argv) == 2:
+    command = sys.argv[1]
+    data = load_data(SAVED_DATA)
+
+    if command == "save":
+        key = input("Enter a key: ")
+        data[key] = clipboard.paste()
+        save_data(SAVED_DATA, data)
+        print("Data saved!")
+    elif command == "load":
+        key = input("Enter a key: ")
+        if key in data:
+            clipboard.copy(data[key])
+            print("Data copied to clipboard.")
+        else:
+            print("Key does not exist.")
+    elif command == "list":
+        print(data)
+    else:
+        print("Unknown command")
+else:
+    print("Please pass exactly one command.")
